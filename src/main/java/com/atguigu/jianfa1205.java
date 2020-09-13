1c3bbdebd2a7d67a3c02e12e792544b1900bf6b1
zhangchen <zhangchen@163.com>
2020-09-13 17:01:41 +0800

    BBBBBBBBBBBB

--- src/main/java/com/atguigu/jianfa1205.java
+++ src/main/java/com/atguigu/jianfa1205.java
@@ -4,5 +4,10 @@ public class jianfa1205 {
     public static void main(String[] args) {
 
         System.out.println("hello   dugujiujian.....");
+        System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+        System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+        System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+        System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+        System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
     }
 }
